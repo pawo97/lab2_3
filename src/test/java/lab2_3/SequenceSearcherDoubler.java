07df2283ced9fed64588eed36d41610a0c1276b5
marax97 <pa-wo97@o2.pl>
2019-03-31 14:48:34 +0200

    Test dla pustej i pelenj sekwencji, oraz test dla tych samych sekwencji

--- src/test/java/lab2_3/SequenceSearcherDoubler.java
+++ src/test/java/lab2_3/SequenceSearcherDoubler.java
@@ -9,7 +9,11 @@ public class SequenceSearcherDoubler implements SequenceSearcher {
     @Override
     public SearchResult search(int key, int[] seq) {
         Builder builder = SearchResult.builder();
-
+        for (int elem : seq) {
+            if (elem == key) {
+                builder.withFound(true);
+            }
+        }
         return builder.build();
     }
 
--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -19,4 +19,19 @@ public class SimilarityFinderTest {
         assertThat(similarity, is(1d));
     }
 
+    @Test
+    public void isReturnedZeroIfOneOfSequencesIsEmpty() {
+        int[] seq1 = new int[0];
+        int[] seq2 = new int[] {1, 2, 3};
+        double similarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(similarity, is(0d));
+    }
+
+    @Test
+    public void isReturnedOneIfSequencesAreTheSame() {
+        int[] seq1 = new int[] {1, 2, 3};
+        int[] seq2 = new int[] {1, 2, 3};
+        double similarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(similarity, is(1d));
+    }
 }
